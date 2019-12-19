e1cc826dad970c8c84f746de78a90a33ba40d1f9
John Boyle <johnboyle@Johns-MacBook-Pro-2.local>
2019-12-18 16:53:27 -0800

    Updates

---
+++ precache-manifest.6891255dbb3c478d8fac74ab1374e06a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add52a831821ee57f4491286a31760b2",
+    "url": "/tweetmap/index.html"
+  },
+  {
+    "revision": "fa4bd3f553b6ef86e566",
+    "url": "/tweetmap/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "fa4bd3f553b6ef86e566",
+    "url": "/tweetmap/static/js/2.772f11b6.chunk.js"
+  },
+  {
+    "revision": "c9ccefdfe14ba3034c96f701899aec01",
+    "url": "/tweetmap/static/js/2.772f11b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b8332685964bfccb9b2",
+    "url": "/tweetmap/static/js/main.cc3934c8.chunk.js"
+  },
+  {
+    "revision": "79afa12940b4c136a1fb",
+    "url": "/tweetmap/static/js/runtime-main.3a6cb72c.js"
+  }
+]);
